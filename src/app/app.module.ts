c3b2b85041ccb7428eeb21693d747d6a28a703e1
Bandi069 <bandivenu89@gmail.com>
2020-06-23 22:31:04 +0530

    [Venu]Add.Initial commit,Adding Book Store Angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './Component/registration/registration.component';
+import { LoginPageComponent } from './Component/login-page/login-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
