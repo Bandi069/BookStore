b959d3d04885afbd54134daf179304033c78bef5
Bandi069 <bandivenu89@gmail.com>
2020-06-24 18:49:10 +0530

    [Venu]Refactor.Implementation of logger in startup

--- BookStoreBackEnd/Startup.cs
+++ BookStoreBackEnd/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookStoreModelLayer.BookManager;
+using BookStoreModelLayer.IBookManager;
 using BookStoreRepositoryLayer.IRepository;
+using BookStoreRepositoryLayer.Logger;
 using BookStoreRepositoryLayer.Repository;
 using BookStoreRepositoryLayer.UserContext;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -18,6 +22,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using NLog;
 using Swashbuckle.AspNetCore.Filters;
 using Swashbuckle.AspNetCore.Swagger;
 
@@ -27,6 +32,7 @@ namespace BookStoreBackEnd
     {
         public Startup(IConfiguration configuration)
         {
+          //  LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "nlog.config"));
             Configuration = configuration;
         }
 
@@ -39,10 +45,11 @@ namespace BookStoreBackEnd
             services.AddDbContextPool<Context>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("UserDBConncetion")));
             services.AddTransient<Context>();
-          //  services.AddTransient<IAccountManager, AccountManager>();
+            services.AddTransient<IAccountManager, AccountManager>();
             services.AddTransient<IBookAccount, BookAccount>();
             services.Configure<DataProtectionTokenProviderOptions>(opt =>
            opt.TokenLifespan = TimeSpan.FromHours(2));
+            services.AddSingleton<ILog, LogNLog>();
 
             var key = Configuration["Jwt:secretKey"];
             var symmetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
@@ -102,6 +109,7 @@ namespace BookStoreBackEnd
             {
                 app.UseHsts();
             }
+           // env.ConfigureNLog("nlog.config");
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
