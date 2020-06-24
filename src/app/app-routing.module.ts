bbacf629ac11d01e7c4e89bfe59150959612e22b
Bandi069 <bandivenu89@gmail.com>
2020-06-24 18:40:27 +0530

    [Venu]Refactor.Adding register,login paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './Component/registration/registration.component';
+import { LoginPageComponent } from './Component/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component:RegistrationComponent},
+  {path:'loginpage',component:LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
