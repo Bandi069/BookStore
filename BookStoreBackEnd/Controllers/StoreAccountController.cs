8b58a3a214cde2f183fecba7f7d7b97a6c47f517
Bandi069 <bandivenu89@gmail.com>
2020-06-24 18:42:34 +0530

    [Venu]Refactor.changes in registration controller

--- BookStoreBackEnd/Controllers/StoreAccountController.cs
+++ BookStoreBackEnd/Controllers/StoreAccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStoreModelLayer.AccountModel;
+using BookStoreModelLayer.IBookManager;
+using BookStoreRepositoryLayer.Logger;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,42 @@ namespace BookStoreBackEnd.Controllers
 {
     public class StoreAccountController: ControllerBase
     {
+        private ILog logger;
+        private readonly IAccountManager accountManager;
+        public StoreAccountController(IAccountManager account,ILog log)
+        {
+            this.accountManager = account;
+            this.logger = log;
+        }
+        [HttpPost]
+        [Route("storeaccount")]
+        public async Task<IActionResult> Register([FromBody] BookRegistrationModel bookRegistrationModel)
+        {
+            try
+            {
+                var result = await this.accountManager.StoreRegistration(bookRegistrationModel);
+                return Ok(new { result });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
 
+        }
+
+        [HttpPost]
+        [Route("storelogin")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            try
+            {
+                var result = await this.accountManager.Login(loginModel);
+                return Ok(new { result });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
