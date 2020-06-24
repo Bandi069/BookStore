a540a0ce043ecc9147ba95cb20af745c79eee34e
Bandi069 <bandivenu89@gmail.com>
2020-06-24 09:30:00 +0530

    [Venu]Refactor.Implementation of startup

--- BookStoreBackEnd/Startup.cs
+++ BookStoreBackEnd/Startup.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using BookStoreRepositoryLayer.IRepository;
+using BookStoreRepositoryLayer.Repository;
+using BookStoreRepositoryLayer.UserContext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BookStoreBackEnd
 {
@@ -26,6 +34,40 @@ namespace BookStoreBackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContextPool<Context>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("UserDBConncetion")));
+            services.AddTransient<Context>();
+          //  services.AddTransient<IAccountManager, AccountManager>();
+            services.AddTransient<IBookAccount, BookAccount>();
+            services.Configure<DataProtectionTokenProviderOptions>(opt =>
+           opt.TokenLifespan = TimeSpan.FromHours(2));
+
+            var key = Configuration["Jwt:secretKey"];
+            var symmetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            services.AddAuthentication(op =>
+            {
+                op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(op =>
+            {
+                op.SaveToken = true;
+                op.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:url"],
+                    ValidAudience = Configuration["Jwt:url"],
+                    IssuerSigningKey = symmetricKey,
+                    RequireExpirationTime = true
+                };
+            });
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
