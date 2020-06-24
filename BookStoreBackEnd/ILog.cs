5c3fd5dd1d02afe37eed8d1aac8fb71aec22756c
Bandi069 <bandivenu89@gmail.com>
2020-06-24 18:43:12 +0530

    [Venu]Add.Adding ILog file

---
+++ BookStoreBackEnd/ILog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreRepositoryLayer.Logger
+{
+    public interface ILog
+    {
+        /// <summary>
+        /// Informations the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Information(string message);
+        void Warning(string message);
+        void Debug(string message);
+        void Error(string message);
+    }
+}
