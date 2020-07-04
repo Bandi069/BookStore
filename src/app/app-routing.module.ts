63b3ade624045e3f7ad3aa442343fb8d6b74b631
Bandi069 <bandivenu89@gmail.com>
2020-07-04 23:16:13 +0530

    [Venu]Refactor.modified app-routing module ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegistrationComponent } from './Component/registration/registration.component';
+import {StoreRoutingModule} from './store-routing.module';
 import { LoginPageComponent } from './Component/login-page/login-page.component';
+import { StoreDashboardComponent } from './Component/store-dashboard/store-dashboard.component';
+import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
+import { AuthenticationGuard } from './authentication.guard';
+import { RegistrationComponent } from './Component/registration/registration.component';
+import { BooksDisplayComponent } from './Component/books-display/books-display.component';
+import { BooksShowComponent } from './Component/books-show/books-show.component';
 
 const routes: Routes = [
-  {path:'register',component:RegistrationComponent},
-  {path:'loginpage',component:LoginPageComponent}
+  { 
+    path: '', 
+    redirectTo: "/loginpage",
+     pathMatch: 'full'
+ },  
+  {
+    path:'loginpage',
+    component:LoginPageComponent
+  },
+ 
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
 ];
 
 @NgModule({
@@ -13,3 +31,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LoginPageComponent,RegistrationComponent,
+  StoreDashboardComponent,PageNotFoundComponent,BooksDisplayComponent]
