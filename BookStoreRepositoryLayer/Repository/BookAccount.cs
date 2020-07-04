609e646376bc10a7d3dc1ce6c8ef8843828d02f6
Bandi069 <bandivenu89@gmail.com>
2020-07-04 22:47:20 +0530

    [Venu]Add.adding bookstore account repo cs

--- BookStoreRepositoryLayer/Repository/BookAccount.cs
+++ BookStoreRepositoryLayer/Repository/BookAccount.cs
@@ -2,8 +2,12 @@
 using BookStoreRepositoryLayer.IRepository;
 using BookStoreRepositoryLayer.UserContext;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,31 +16,77 @@ namespace BookStoreRepositoryLayer.Repository
     public class BookAccount : IBookAccount
     {
         private readonly Context userContext;
-        private readonly IConfiguration configuaration;
-        public BookAccount(Context context, IConfiguration configuaration)
+        private readonly IConfiguration configuration;
+        public BookAccount(Context context, IConfiguration configuration)
         {
             this.userContext = context;
+            this.configuration = configuration;
 
         }
-        public Task<int> StoreRegistration(BookRegistrationModel bookRegistrationModel)
+        public async Task<int> StoreRegistration(BookRegistrationModel bookRegistrationModel)
         {
             BookRegistrationModel bookcred = new BookRegistrationModel()
             {
-                FirstName =bookRegistrationModel.FirstName,
-                LastName=bookRegistrationModel.LastName,
-                Email=bookRegistrationModel.Email,
-                Password=bookRegistrationModel.Password,
-                HashCode=bookRegistrationModel.HashCode
+                Id = bookRegistrationModel.Id,
+                FirstName = bookRegistrationModel.FirstName,
+                LastName = bookRegistrationModel.LastName,
+                Email = bookRegistrationModel.Email,
+                Password = bookRegistrationModel.Password,
             };
+            var password = PasswordEncryption.EncodePasswordMd5(bookRegistrationModel.Password);
+            bookcred.Password = password;
             var registeruser = this.userContext.StoreAccount.Add(bookcred);
             var result = this.userContext.SaveChanges();
-            return Task.Run(()=>result);
+            var rest = await Task.Run(() => result);
+            return rest;
         }
-        public Task<string> StoreLogin(LoginModel loginModel)
+        public string StoreLogin(LoginModel loginModel)
         {
-            return null;
+            if (FindEmail(loginModel.Email, loginModel.Password))
+            {
+                var token = this.JwtToken(loginModel);
+                return token;
+            }
+            else
+            {
+                return null;
+            }
         }
+        public string JwtToken(LoginModel loginModel)
+        {
+            var claims = new[]
+              {
+                new Claim(JwtRegisteredClaimNames.Sub,loginModel.Email),
+                new Claim(JwtRegisteredClaimNames.Sub,loginModel.Password),
+            };
+            var key = configuration["Jwt:secretKey"];
+            var secretkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var SigningCredentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:url"],
+                audience: configuration["Jwt:url"],
+                 claims,
+                expires: DateTime.Now.AddHours(24),
+                signingCredentials: SigningCredentials);
 
+            var securitytoken = new JwtSecurityTokenHandler().WriteToken(token);
+            this.userContext.SaveChanges();
+            return securitytoken;
+        }
+        public bool FindEmail(string email, string password)
+        {
+            var result = this.userContext.StoreAccount.Where(opt => opt.Email == email).SingleOrDefault();
+            if (result != null)
+            {
+                var encrptpassword = PasswordEncryption.EncodePasswordMd5(password);
+                var res = this.userContext.StoreAccount.Where(opt => opt.Email == email && opt.Password == encrptpassword).SingleOrDefault();
+                if (res != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
     }
 }
