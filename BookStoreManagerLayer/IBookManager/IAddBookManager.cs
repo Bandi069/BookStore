f7de969438c196343ff5d80c5956d5457edc51f4
Bandi069 <bandivenu89@gmail.com>
2020-07-04 22:50:25 +0530

    [Venu]Refactor.Implemention of store add book manager

---
+++ BookStoreManagerLayer/IBookManager/IAddBookManager.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreModelLayer.AddBookModel;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreManagerLayer.IBookManager
+{
+   public interface IAddBookManager
+    {
+        Task<string> Addbook(AddBookModel addBookModel);
+        Task<string> UploadImg(int BookId, IFormFile Image);
+        List<AddBookModel> Getbook();
+        Task Delete(int BookId);
+        int CountBook();
+        Task Update(AddBookModel addBookModel);
+    }
+}
