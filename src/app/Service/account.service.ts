fe609d2d554d4edc7afc92eebf49dd32a2b539a3
Bandi069 <bandivenu89@gmail.com>
2020-07-04 23:11:22 +0530

    [Venu]Refactor.Modified account service ts file

--- src/app/Service/account.service.ts
+++ src/app/Service/account.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+ // debugger;
+  registrationform(values) {
+   //debugger;
+    return this.http.post(environment.Url + 'api/StoreAccount', values);
+  }
+  loginform(parm) {
+   // debugger;
+    return this.http.post<any>(environment.Url + 'api/StoreAccount/StoreLogin', parm);
+  }
 }
