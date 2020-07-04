547f8d435948db9633c551a53e621c183d1bc606
Bandi069 <bandivenu89@gmail.com>
2020-07-04 22:50:42 +0530

    [Venu]Refactor.Implemention of store add cart manager

---
+++ BookStoreManagerLayer/IBookManager/ICartManager.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreModelLayer.AddBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreManagerLayer.IBookManager
+{
+    public interface ICartManager
+    {
+        Task<string> AddCart(AddCartModel addCart);
+        Task UpdateCart(AddCartModel addCart);
+        Task DeleteCart(int id);
+        int CartCount();
+        IQueryable GetAllCartValue();
+        IQueryable GetAllCart(int id);
+    }
+}
