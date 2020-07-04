acc2fa47d070a2eca2d5d5f8a1dbc5873ddd7261
Bandi069 <bandivenu89@gmail.com>
2020-07-04 22:51:07 +0530

    [Venu]Refactor.Implemention of store customer details manager

---
+++ BookStoreManagerLayer/IBookManager/ICustomerDetailsManager.cs
@@ -0,0 +1,18 @@
+﻿using BookStoreModelLayer.AccountModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreManagerLayer.IBookManager
+{
+    public interface ICustomerDetailsManager
+    {
+        Task<string> NewCustomerDetails(CustomerDetailsModel newCustomer);
+        List<CustomerDetailsModel> GetCustomerDetails();
+
+        Task<string> UpdateCustomerDetails(CustomerDetailsModel customerDetailsModel);
+
+        Task DeleteCustomerDetails(int id);
+    }
+}
